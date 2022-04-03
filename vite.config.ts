0998239715ca1858e813499b5e2cf68d1f9adc4f
xiaohuiji <6853947+fuyucn@users.noreply.github.com>
2022-04-03 02:33:37 -0700

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import legacy from "@vitejs/plugin-legacy";
 
 export default defineConfig({
   plugins: [
     vue({
       reactivityTransform: true
+    }),
+    legacy({
+      targets: ["defaults", "not IE 11"]
     })
   ]
 });
